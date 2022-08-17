47ac7aabc222152daa91a038cd4deca0713faa8d
Brayan Wilis <BrayanWilis@outlook.com>
2022-08-16 22:42:41 -0300

    [ Feat: Entity ]: Added Most of the entities of the system

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/table/TableEntity.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/table/TableEntity.java
@@ -1,4 +1,41 @@
 package com.project.uniamerica.descomplica.backend.table;
+import javax.persistence.*;
 
+@Entity
 public class TableEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TableEntity() {}
+
+    public TableEntity(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TableEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- backend/src/main/java/com/project/uniamerica/descomplica/backend/tipol/TipoEntity.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/tipol/TipoEntity.java
@@ -1,4 +1,17 @@
 package com.project.uniamerica.descomplica.backend.tipol;
+import javax.persistence.*;
 
+@Entity
 public class TipoEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
