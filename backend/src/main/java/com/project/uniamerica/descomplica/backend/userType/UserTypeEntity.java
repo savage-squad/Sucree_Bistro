90bdc6d376727b56641d6c82546b767c7cda5da5
cleyberfaedo <cleyberfaedocc@hotmail.com>
2022-08-17 22:30:37 -0300

    create api UserTypeEntity

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/userType/UserTypeEntity.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/userType/UserTypeEntity.java
@@ -1,4 +1,21 @@
 package com.project.uniamerica.descomplica.backend.userType;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class UserTypeEntity {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String name;
+
+
 }
