6b0b81499de0ec7d4983a2137ea76da6fa145917
Ducilio <62483330+DucilioManjate@users.noreply.github.com>
2022-08-24 08:04:45 -0300

    userResponse

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/auth/UserResource.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/auth/UserResource.java
@@ -1,4 +1,33 @@
 package com.project.uniamerica.descomplica.backend.auth;
 
+import com.project.uniamerica.descomplica.backend.exception.ObjectNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class UserResource {
+
+    @Autowired
+    private UserImplementService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable(value="id") Long id) {
+
+        ModelMapper modelMapper = new ModelMapper();
+        UserEntity userEntity = service.findById(id);
+
+        if(userEntity == null) {
+            throw new ObjectNotFoundException("Object "+UserEntity.class.getName()+" not found! id "+id);
+        }
+
+        UserDto userDto = modelMapper.map(userEntity,UserDto.class);
+        return ResponseEntity.ok().body(userDto);
+    }
+
 }
