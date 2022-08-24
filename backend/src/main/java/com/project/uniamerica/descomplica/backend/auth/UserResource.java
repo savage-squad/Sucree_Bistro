8dc1528d825547d1b65f0e6491d1223da8950244
Ducilio <62483330+DucilioManjate@users.noreply.github.com>
2022-08-24 08:15:53 -0300

    UserResource

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/auth/UserResource.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/auth/UserResource.java
@@ -4,11 +4,16 @@ import com.project.uniamerica.descomplica.backend.exception.ObjectNotFoundExcept
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin("*")
 @RestController
@@ -45,5 +50,33 @@ public class UserResource {
                 .toUri();
         return ResponseEntity.created(uri).build();
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDto dto, @PathVariable(value="id") Long id){
+        ModelMapper modelMapper = new ModelMapper();
+        UserEntity obj = modelMapper.map(dto,UserEntity.class);
+        obj.setId(id);
+        obj = this.service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable(value="id") Long id) {
+        service.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
 
 }
