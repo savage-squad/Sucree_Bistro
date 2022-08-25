6f176dddcd41d04cf764fe8821013f7067333e68
Khensane Paulo <90350372+khensanepaulo@users.noreply.github.com>
2022-08-25 15:28:33 -0300

    bug-correction

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/order/OrderRepository.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/order/OrderRepository.java
@@ -1,13 +1,12 @@
 package com.project.uniamerica.descomplica.backend.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface OrderRepository extends JpaRepository<RequestEntity, UUID> {
+public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
 
 }
 
--- backend/src/main/java/com/project/uniamerica/descomplica/backend/order/OrderService.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/order/OrderService.java
@@ -4,27 +4,32 @@ package com.project.uniamerica.descomplica.backend.order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
-//
-//    @Transactional
-//    public OrderEntity save(OrderEntity orderEntity){
-//        return orderRepository.save(orderEntity);
-//    }
-//    public List<OrderEntity> findAll() {
-//        return orderRepository.findAll();
-//    }
-//
-//    public Optional<OrderEntity> findById(UUID id) {
-//        return orderRepository.findById(id);
-//    }
-//
-//    @Transactional
-//    public void delete(
-//            OrderEntity OrderEntity) {
-//        orderRepository.delete(OrderEntity);
-//    }
+
+    @Transactional
+    public OrderEntity save(OrderEntity orderEntity){
+        return orderRepository.save(orderEntity);
+    }
+    public List<OrderEntity> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<OrderEntity> findById(UUID id) {
+        return orderRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(
+            OrderEntity OrderEntity) {
+        orderRepository.delete(OrderEntity);
+    }
 }
