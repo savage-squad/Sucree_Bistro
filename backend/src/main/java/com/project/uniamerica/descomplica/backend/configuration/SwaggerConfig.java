5bd35c3387f8d0820a62a12acb3fc5e3ec816378
Khensane Paulo <90350372+khensanepaulo@users.noreply.github.com>
2022-09-20 19:29:32 -0300

    erros

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/configuration/SwaggerConfig.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/configuration/SwaggerConfig.java
@@ -4,14 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -23,25 +18,10 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(metaInfo());
-    }
-
-    private ApiInfo metaInfo(){
-        ApiInfo apiInfo = new ApiInfo(
-                "Sucree Bistro API REST",
-                "API REST de SecreeBistro.",
-                "1.0",
-                "Terms of Service",
-                new Contact(
-                        "Khensane Paulo",
-                        "https://www.linkedin.com/in/khensane-paulo-079594227",
-                        "khensanepaulo99@gmail.com"),
-                        "Apache License version 2.0",
-                        "https://www.apache.org/license.html", new ArrayList<VendorExtension>()
-        );
-        return apiInfo;
+                .build();
 
     }
+
+
 
     }
