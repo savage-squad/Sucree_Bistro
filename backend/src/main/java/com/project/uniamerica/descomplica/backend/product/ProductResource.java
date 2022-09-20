c51c351564b327eb3d64c952e0c279e9e7ddd87b
Ducilio <62483330+DucilioManjate@users.noreply.github.com>
2022-09-20 18:51:21 -0300

    resolve

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/product/ProductResource.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/product/ProductResource.java
@@ -1,61 +1,73 @@
-package com.project.uniamerica.descomplica.backend.product;
+//package com.project.uniamerica.descomplica.backend.product;
 
-public class ProductResource {
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
+import java.util.Optional;
 
-    final ProductService productService;
-
-    @Autowired
-    public ProductResource(ProductService productService) {
-        this.productService = productService;
-    }
-
-    @GetMapping()
-    public ResponseEntity<List<ProductEntity>> getAllProduct() {
-        return ResponseEntity.status(HttpStatus.OK).body(productService.findAll());
-    }
-
-    @PostMapping
-    public ResponseEntity<Object> saveProduct(@RequestBody @Valid ProductDto productDto) {
-
-        var productEntity = new ProductEntity();
-        BeanUtils.copyProperties(productDto, productEntity);
-        return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(productEntity));
-    }
+//public class ProductResource {
 
 
+//    final ProductService productService;
+//
+//    @Autowired
+//    public ProductResource(ProductService productService) {
+//        this.productService = productService;
+//    }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getOneproduct(@PathVariable(value = "id") UUID id) {
-        Optional<ProductEntity> productEntityOptional = productService.findById(id);
-          if (!productEntityOptional.isPresent()) {
-              return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found.");
-          }
-        return ResponseEntity.status(HttpStatus.OK).body(productEntityOptional.get());
-    }
+//    @GetMapping()
+//    public ResponseEntity<Object> getAllProduct() {
+//        return ResponseEntity.status(HttpStatus.OK).body(productService.findAll());
+//    }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteproduct(@PathVariable(value = "id") UUID id) {
-        Optional<ProductEntity> productEntityOptional = productService.findById(id);
-        if (!productEntityOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found.");
-        }
-        productService.delete(productEntityOptional.get());
-        return ResponseEntity.status(HttpStatus.OK).body("product deleted sucessfully");
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateproduct(@PathVariable(value = "id") UUID id,
-                                                @RequestBody @Valid ProductDto productDto) {
-        Optional<ProductEntity> productEntityOptional = productService.findById(id);
-        if (!productEntityOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found.");
-        }
-        var productEntity = new ProductEntity();
-        BeanUtils.copyProperties(productDto, productEntity);
-        productEntity.setId(productEntityOptional.get().getId());
-        return ResponseEntity.status(HttpStatus.OK).body(productService.save(productEntity));
-    }
+//    @PostMapping
+//    public ResponseEntity<Object> saveProduct(@RequestBody @Valid ProductDto productDto) {
+//
+//        var productEntity = new ProductEntity();
+//        BeanUtils.copyProperties(productDto, productEntity);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(productEntity));
+//    }
 
 
-}
+
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Object> getOneproduct(@PathVariable(value = "id") UUID id) {
+//        Optional<ProductEntity> productEntityOptional = productService.findById(id);
+//          if (!productEntityOptional.isPresent()) {
+//              return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found.");
+//          }
+//        return ResponseEntity.status(HttpStatus.OK).body(productEntityOptional.get());
+//    }
+
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Object> deleteproduct(@PathVariable(value = "id") UUID id) {
+//        Optional<ProductEntity> productEntityOptional = productService.findById(id);
+//        if (!productEntityOptional.isPresent()) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found.");
+//        }
+//        productService.delete(productEntityOptional.get());
+//        return ResponseEntity.status(HttpStatus.OK).body("product deleted sucessfully");
+//    }
+
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Object> updateproduct(@PathVariable(value = "id") UUID id,
+//                                                @RequestBody @Valid ProductDto productDto) {
+//        Optional<ProductEntity> productEntityOptional = productService.findById(id);
+//        if (!productEntityOptional.isPresent()) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found.");
+//        }
+//        var productEntity = new ProductEntity();
+//        BeanUtils.copyProperties(productDto, productEntity);
+//        productEntity.setId(productEntityOptional.get().getId());
+//        return ResponseEntity.status(HttpStatus.OK).body(productService.save(productEntity));
+//    }
+
+
+//}
--- backend/src/main/java/com/project/uniamerica/descomplica/backend/product/ProductService.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/product/ProductService.java
@@ -1,4 +1,5 @@
 package com.project.uniamerica.descomplica.backend.product;
 
 public class ProductService {
+
 }
