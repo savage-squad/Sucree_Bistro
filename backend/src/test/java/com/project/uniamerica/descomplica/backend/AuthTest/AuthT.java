557ef6fc68430583b06e0adb30bce691c6c246fb
Ducilio <62483330+DucilioManjate@users.noreply.github.com>
2022-09-20 18:29:45 -0300

    resolve

--- backend/src/test/java/com/project/uniamerica/descomplica/backend/AuthTest/AuthT.java
+++ backend/src/test/java/com/project/uniamerica/descomplica/backend/AuthTest/AuthT.java
@@ -12,19 +12,27 @@
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.http.ResponseEntity;
-//import org.springframework.test.annotation.DirtiesContext;
-//
-//import java.util.List;
-//import java.util.Locale;
-//import java.util.Optional;
-//import java.util.Random;
-//
-//@SpringBootTest
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-//public class AuthT {
-//    @Autowired
-//    private UserImplementService userImplementService;
-//    private final Faker faker = new Faker(new Locale("pt-br"));
+import com.github.javafaker.Faker;
+import com.project.uniamerica.descomplica.backend.auth.Role;
+import com.project.uniamerica.descomplica.backend.auth.UserEntity;
+import com.project.uniamerica.descomplica.backend.auth.UserImplementService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.Random;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class AuthT {
+    @Autowired
+    private UserImplementService userImplementService;
+    private final Faker faker = new Faker(new Locale("pt-br"));
 //
 //    {/*
 //    @Test
@@ -43,16 +51,16 @@
 //
 //    }*/}
 //
-//    @Test
-//    void store() {
-//        UserEntity user = new UserEntity();
-//        int role = new Random().nextInt(Role.values().length);
-//
-//        user.setEmail(faker.internet().emailAddress());
-//        user.setPassword(faker.random().hex());
-//        user.setRole(Role.values()[role]);
-//        UserEntity resultdb = userImplementService.insert(user);
-//        Assertions.assertEquals(user, resultdb);
-//    }
-//
-//}
+    @Test
+    void store() {
+        UserEntity user = new UserEntity();
+        int role = new Random().nextInt(Role.values().length);
+
+        user.setEmail(faker.internet().emailAddress());
+        user.setPassword(faker.random().hex());
+        user.setRole(Role.values()[role]);
+        UserEntity resultdb = userImplementService.insert(user);
+        Assertions.assertEquals(user, resultdb);
+    }
+
+}
--- backend/src/test/java/com/project/uniamerica/descomplica/backend/Integracao/UserIntegrationTest.java
+++ backend/src/test/java/com/project/uniamerica/descomplica/backend/Integracao/UserIntegrationTest.java
@@ -1,77 +1,77 @@
-package com.project.uniamerica.descomplica.backend.Integracao;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.uniamerica.descomplica.backend.auth.Role;
-import com.project.uniamerica.descomplica.backend.auth.UserEntity;
-import com.project.uniamerica.descomplica.backend.auth.UserResource;
-import org.hamcrest.Matchers;
-import org.junit.Before;
-import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.regex.Matcher;
-
-import static com.project.uniamerica.descomplica.backend.auth.Role.RECEPTIONIST;
-
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureTestDatabase
-public class UserIntegrationTest{
-
-    private MockMvc mockMvc;
-
-    @Autowired
-    private UserResource userResource;
-
-    @Before
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(userResource).build();
-    }
-
-    @Test
-    public void createUserInDatabse_ReturnStatusCodeOk() throws Exception {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("liomanjate@gmail.com");
-        userEntity.setPassword("1234");
-        userEntity.setRole(RECEPTIONIST);
-        ObjectMapper mapper = new ObjectMapper();
-
-        String json = mapper.writeValueAsString(userEntity);
-
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.handler()
-                        .toString("location", Matchers.containsString("https://localhost:8080/api/auth")));
-    }
-    @Test
-    public void createUserInCorrects_ReturnStatusCode400() throws Exception {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("liomanjate@gmail.com");
-        userEntity.setPassword("1234");
-        userEntity.setRole(RECEPTIONIST);
-        ObjectMapper mapper = new ObjectMapper();
-
-        String json = mapper.writeValueAsString(userEntity);
-
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(""))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
-    }
-
-
-}
+//package com.project.uniamerica.descomplica.backend.Integracao;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.project.uniamerica.descomplica.backend.auth.Role;
+//import com.project.uniamerica.descomplica.backend.auth.UserEntity;
+//import com.project.uniamerica.descomplica.backend.auth.UserResource;
+//import org.hamcrest.Matchers;
+//import org.junit.Before;
+//import org.junit.jupiter.api.*;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.test.web.server.LocalServerPort;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MockMvcBuilder;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//
+//import java.util.regex.Matcher;
+//
+//import static com.project.uniamerica.descomplica.backend.auth.Role.RECEPTIONIST;
+//
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureTestDatabase
+//public class UserIntegrationTest{
+//
+//    private MockMvc mockMvc;
+//
+//    @Autowired
+//    private UserResource userResource;
+//
+//    @Before
+//    public void setup() {
+//        this.mockMvc = MockMvcBuilders.standaloneSetup(userResource).build();
+//    }
+//
+//    @Test
+//    public void createUserInDatabse_ReturnStatusCodeOk() throws Exception {
+//        UserEntity userEntity = new UserEntity();
+//        userEntity.setEmail("liomanjate@gmail.com");
+//        userEntity.setPassword("1234");
+//        userEntity.setRole(RECEPTIONIST);
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        String json = mapper.writeValueAsString(userEntity);
+//
+//        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(json))
+//                .andExpect(MockMvcResultMatchers.status().isCreated())
+//                .andExpect(MockMvcResultMatchers.handler()
+//                        .toString("location", Matchers.containsString("https://localhost:8080/api/auth")));
+//    }
+//    @Test
+//    public void createUserInCorrects_ReturnStatusCode400() throws Exception {
+//        UserEntity userEntity = new UserEntity();
+//        userEntity.setEmail("liomanjate@gmail.com");
+//        userEntity.setPassword("1234");
+//        userEntity.setRole(RECEPTIONIST);
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        String json = mapper.writeValueAsString(userEntity);
+//
+//        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/auth")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(""))
+//                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+//    }
+//
+//
+//}
