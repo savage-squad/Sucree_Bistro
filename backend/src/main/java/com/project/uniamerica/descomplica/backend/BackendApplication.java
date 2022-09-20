7e6f377cb3649e3de700894705348ab07cefb74f
Ducilio <62483330+DucilioManjate@users.noreply.github.com>
2022-09-20 19:08:24 -0300

    @Anotations

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/BackendApplication.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/BackendApplication.java
@@ -3,9 +3,11 @@ package com.project.uniamerica.descomplica.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableWebMvc
 @SpringBootApplication
+@EnableSwagger2
 public class BackendApplication {
 
     public static void main(String[] args) {
