3954f8994288f63c9c36ef64f0f896ec1e88071b
Khensane Paulo <90350372+khensanepaulo@users.noreply.github.com>
2022-09-28 17:06:30 -0300

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,18 @@ import {
   theme,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import SignInPage from "./pages/Login/SignIn"
+//import SignInPage from "./pages/Login/SignIn"
 import FooterComponents from "./components/_ui/Footer"
-
+import HeaderViewComponent from "./components/_ui/Header/navbar"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <SignInPage/>
-        <FooterComponents/>
+        {/* <SignInPage/> */}
+        <HeaderViewComponent colorScheme={""} size={"sm"} variant={""} />
+        <FooterComponents />
       </Grid>
     </Box>
   </ChakraProvider>
