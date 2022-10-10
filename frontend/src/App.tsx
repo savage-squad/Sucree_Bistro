f7e66d05c972cce291fede6a588ff776fbd44832
DucilioManjate <liomanjate@gmail.com>
2022-10-10 17:09:23 -0400

    SignInPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import {
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import RoutesNavegationPage from "../router/index"
 import SignInPage from "./pages/Login/SignIn"
+import SignOutPage from "./pages/Login/SigOut"
 
 
 
@@ -17,7 +18,8 @@ export const App = () => (
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <SignInPage/>
+        {/* <SignInPage/> */}
+        <SignOutPage/>
         {/* <RoutesNavegationPage /> */}
   
       </Grid>
