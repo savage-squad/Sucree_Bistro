e9a829fe9940c1b2f7b856b734760217f591f583
DucilioManjate <liomanjate@gmail.com>
2022-10-24 19:53:51 -0300

    cool

--- frontend-next/src/components/slick/index.tsx
+++ frontend-next/src/components/slick/index.tsx
@@ -5,15 +5,11 @@ import Slider from "react-slick";
 
 export default class AutoPlay extends Component {
 
-     property = {
-        imageUrl: 'https://bit.ly/2Z4KKcF',
-        imageAlt: 'Rear view of modern home with pool',
-        beds: 3,
-        baths: 2,
-        title: 'Modern home in city center in the heart of historic Los Angeles',
-        formattedPrice: '$1,900.00',
-        reviewCount: 34,
-        rating: 4,
+    property = {
+        imageUrl: 'https://i.pinimg.com/736x/45/fb/18/45fb1836a16b92562589bb9ccabfb90c.jpg',
+    }
+    bebidas = {
+        imageUrl: 'https://i.pinimg.com/736x/45/fb/18/45fb1836a16b92562589bb9ccabfb90c.jpg',
     }
 
     render() {
@@ -23,7 +19,7 @@ export default class AutoPlay extends Component {
             slidesToShow: 3,
             slidesToScroll: 1,
             autoplay: true,
-            speed: 2000,
+            speed: 1000,
             autoplaySpeed: 2000,
             cssEase: "linear"
         };
@@ -33,55 +29,17 @@ export default class AutoPlay extends Component {
                 <Slider {...settings}>
                     <div>
                         <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                            <Image src={this.property.imageUrl} alt={this.property.imageAlt} />
+                            <Image src={this.property.imageUrl} />
 
                             <Box p='6'>
                                 <Box display='flex' alignItems='baseline'>
                                     <Badge borderRadius='full' px='2' colorScheme='teal'>
-                                        New
+                                        Entrada
                                     </Badge>
-                                    <Box
-                                        color='gray.500'
-                                        fontWeight='semibold'
-                                        letterSpacing='wide'
-                                        fontSize='xs'
-                                        textTransform='uppercase'
-                                        ml='2'
-                                    >
-                                        {this.property.beds} beds &bull; {this.property.baths} baths
-                                    </Box>
+
                                 </Box>
 
-                                <Box
-                                    mt='1'
-                                    fontWeight='semibold'
-                                    as='h4'
-                                    lineHeight='tight'
-                                    noOfLines={1}
-                                >
-                                    {this.property.title}
-                                </Box>
-
-                                <Box>
-                                    {this.property.formattedPrice}
-                                    <Box as='span' color='gray.600' fontSize='sm'>
-                                        / wk
-                                    </Box>
-                                </Box>
-
-                                <Box display='flex' mt='2' alignItems='center'>
-                                    {Array(5)
-                                        .fill('')
-                                        .map((_, i) => (
-                                            <StarIcon
-                                                key={i}
-                                                color={i < this.property.rating ? 'teal.500' : 'gray.300'}
-                                            />
-                                        ))}
-                                    <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-                                        {this.property.reviewCount} reviews
-                                    </Box>
-                                </Box>      </Box>
+                            </Box>
                         </Box>
 
                     </div>
