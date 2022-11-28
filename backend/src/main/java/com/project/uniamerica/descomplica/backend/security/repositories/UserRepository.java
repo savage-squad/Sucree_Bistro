c5b84f3010cfcc40a7e597884d139d39895dfa8b
Khensane Paulo <90350372+khensanepaulo@users.noreply.github.com>
2022-11-28 20:03:37 -0300

    security

---
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/security/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package com.project.uniamerica.descomplica.backend.security.repositories;
+
+
+import java.util.Optional;
+
+import com.project.uniamerica.descomplica.backend.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
