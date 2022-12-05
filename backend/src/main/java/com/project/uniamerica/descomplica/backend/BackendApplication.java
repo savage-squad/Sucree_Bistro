5c9499b78f1e4def0bdbc401307ba1b2718dbb81
Khensane Paulo <90350372+khensanepaulo@users.noreply.github.com>
2022-12-05 09:02:21 -0300

    exclude config

--- backend/src/main/java/com/project/uniamerica/descomplica/backend/BackendApplication.java
+++ backend/src/main/java/com/project/uniamerica/descomplica/backend/BackendApplication.java
@@ -2,11 +2,13 @@ package com.project.uniamerica.descomplica.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-//@EnableWebMvc
-@SpringBootApplication
+@EnableWebMvc
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableSwagger2
 public class BackendApplication {
 
